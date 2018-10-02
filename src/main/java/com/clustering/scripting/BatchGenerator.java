ae0323e441c3852335aabcefbcd6059ede931796
Marwan <zouinkhi.marwan@gmail.com>
2018-10-02 10:34:33 +0200

    fix bugs workflow parts

--- src/main/java/com/clustering/scripting/BatchGenerator.java
+++ src/main/java/com/clustering/scripting/BatchGenerator.java
@@ -13,6 +13,7 @@ public class BatchGenerator {
 		boolean error = false;
 		File file = new File(Config.getTempFolderPath());
 		String filePath = file.getAbsolutePath()+"/submit.cmd";
+		System.out.println("Input total files:"+totalInputFiles+" - Tasks Per job:"+tasksPerJob);
 		int jobs = totalInputFiles / tasksPerJob;
 		int restPortions = totalInputFiles % tasksPerJob;
 		try (PrintWriter out = new PrintWriter(filePath)) {
@@ -20,15 +21,28 @@ public class BatchGenerator {
 		    out.println( "cd " + Config.getClusterPath());
 		    int i=0;
 		    for (i=0;i<jobs;i++){
+		    	if(tasksPerJob>1) {
 		    	if(i==0) {
-		    	out.println("qsub -N \"task_"+(i+1)+"\" -t "+(i*tasksPerJob+1)+"-"+((i+1)*tasksPerJob+1)+" ./task.sh");
+		    	out.println("qsub -N \"task_"+(i+1)+"\" -t "+(i*tasksPerJob+1)+"-"+((i+1)*tasksPerJob)+" ./task.sh");
 		    	}else {
-		    		out.println("qsub -N \"task_"+(i+1)+"\" -t "+(i*tasksPerJob+1)+"-"+((i+1)*tasksPerJob+1)+" -hold_jid task_"+i+" ./task.sh");
+		    		out.println("qsub -N \"task_"+(i+1)+"\" -t "+(i*tasksPerJob+1)+"-"+((i+1)*tasksPerJob)+" -hold_jid task_"+i+" ./task.sh");
+		    	}
+		    	}
+		    	else {
+		    		if(i==0) {
+				    	out.println("qsub -N \"task_"+(i+1)+"\" -t "+(i*tasksPerJob+1)+" ./task.sh");
+				    	}else {
+				    		out.println("qsub -N \"task_"+(i+1)+"\" -t "+(i*tasksPerJob+1)+" -hold_jid task_"+i+" ./task.sh");
+				    	}	
 		    	}
 		    	out.println("qsub -N \"prov_"+(i+1)+"\" -t "+(i+1)+" -hold_jid task_"+(i+1)+" -v uuid="+Config.getUUID()+" ./logProvider.sh");
 		    }
 		    if(restPortions>0) {
-		    	out.println("qsub -N \"task_"+(i+1)+"\" -t "+(i*tasksPerJob+1)+"-"+(i*tasksPerJob+restPortions+1)+" -hold_jid task_"+i+" ./task.sh");
+		    	if((i*tasksPerJob+1)==(i*tasksPerJob+restPortions)) {
+		    		out.println("qsub -N \"task_"+(i+1)+"\" -t "+(i*tasksPerJob+1)+" -hold_jid task_"+i+" ./task.sh");
+		    	}else {
+		    		out.println("qsub -N \"task_"+(i+1)+"\" -t "+(i*tasksPerJob+1)+"-"+(i*tasksPerJob+restPortions)+" -hold_jid task_"+i+" ./task.sh");
+		    	}
 		    	out.println("qsub -N \"prov_"+(i+1)+"\" -t "+(i+1)+" -hold_jid task_"+(i+1)+" -v uuid='"+Config.getUUID()+"' ./logProvider.sh");
 		    }
 		} catch (FileNotFoundException e) {
