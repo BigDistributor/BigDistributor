c1b06fd0da2cf646adddc889bd30af79153acfb0
mzouink <zouinkhi.marwan@gmail.com>
2019-04-26 17:56:05 +0200

    test N5 save

--- src/main/java/net/preibisch/distribution/plugin/testSaveHDF5.java
+++
@@ -1,9 +0,0 @@
-package main.java.net.preibisch.distribution.plugin;
-
-import main.java.net.preibisch.distribution.io.BdvRaiVolumeExport;
-
-public class testSaveHDF5 {
-public static void main(String[] args) {
-	BdvRaiVolumeExport<T> exporter = new BdvRaiVolumeExport<T>();
-}
-}
