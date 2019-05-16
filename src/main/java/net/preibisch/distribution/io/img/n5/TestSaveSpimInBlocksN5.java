22452948736029c52702e911315d03dada0b2613
mzouink <zouinkhi.marwan@gmail.com>
2019-05-16 12:33:22 +0200

    Save working on Spim data. todo check block generator

--- src/main/java/net/preibisch/distribution/io/img/n5/TestSaveSpimInBlocksN5.java
+++ src/main/java/net/preibisch/distribution/io/img/n5/TestSaveSpimInBlocksN5.java
@@ -1,7 +1,8 @@
-package main.java.net.preibisch.distribution.plugin;
+package main.java.net.preibisch.distribution.io.img.n5;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -11,8 +12,11 @@ import org.janelia.saalfeldlab.n5.AbstractDataBlock;
 import org.janelia.saalfeldlab.n5.BlockReader;
 import org.janelia.saalfeldlab.n5.DataBlock;
 import org.janelia.saalfeldlab.n5.DatasetAttributes;
+import org.janelia.saalfeldlab.n5.GzipCompression;
 import org.janelia.saalfeldlab.n5.N5FSReader;
+import org.janelia.saalfeldlab.n5.N5FSWriter;
 import org.janelia.saalfeldlab.n5.N5Reader;
+import org.janelia.saalfeldlab.n5.N5Writer;
 import org.janelia.saalfeldlab.n5.imglib2.N5Utils;
 
 import ij.ImageJ;
@@ -24,8 +28,8 @@ import main.java.net.preibisch.distribution.algorithm.controllers.items.MetaData
 import main.java.net.preibisch.distribution.algorithm.controllers.items.callback.Callback;
 import main.java.net.preibisch.distribution.algorithm.controllers.logmanager.MyLogger;
 import main.java.net.preibisch.distribution.headless.MainJob;
-import main.java.net.preibisch.distribution.io.img.n5.N5IO;
 import main.java.net.preibisch.distribution.tools.Tools;
+import mpicbg.spim.data.SpimDataException;
 import mpicbg.spim.data.sequence.ViewId;
 import net.imglib2.FinalInterval;
 import net.imglib2.Interval;
@@ -33,36 +37,70 @@ import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.util.Util;
+import net.imglib2.view.Views;
 import net.preibisch.mvrecon.fiji.spimdata.SpimData2;
+import net.preibisch.mvrecon.fiji.spimdata.XmlIoSpimData2;
 import net.preibisch.mvrecon.process.fusion.FusionTools;
 import net.preibisch.simulation.imgloader.SimulatedBeadsImgLoader;
 
 public class TestSaveSpimInBlocksN5 {
-	public static void main(String[] args) throws InterruptedException, IOException {
+	public static void main(String[] args) throws InterruptedException, IOException, SpimDataException {
 
 		new ImageJ();
 		MyLogger.initLogger();
 		final String tmpDir = "/home/mzouink/Desktop/testsave/";
-		final String output = tmpDir + "out.n5";
+		final String input_path = "/home/mzouink/Desktop/testn5/dataset.xml";
+		final String output_path = "/home/mzouink/Desktop/testn5/output3.n5";
 
 		Tools.cleanFolder(tmpDir);
 
 		SpimData2 spimData = SpimData2.convert(SimulatedBeadsImgLoader.spimdataExample(new int[] { 0, 0, 0 }));
-
+//		SpimData2 spimData = new XmlIoSpimData2( "" ).load(input_path);
+		
 		final List<ViewId> viewIds = new ArrayList<ViewId>();
 		viewIds.addAll(spimData.getSequenceDescription().getViewDescriptions().values());
 
 		// small part of the bounding box\
-		Interval bb = new FinalInterval(new long[] { 0, 0, 0 }, new long[] { 100, 250, 350 });
+		Interval bb = new FinalInterval(new long[] { 0, 0, 0 }, new long[] { 200, 300, 200 });
 
 		// downsampling
 		double downsampling = Double.NaN; // 2.0
+//		double downsampling = 2; // 2.0
 
 		// perforn the fusion virtually
 		RandomAccessibleInterval<FloatType> source = FusionTools.fuseVirtual(spimData, viewIds, bb, downsampling);
 
 		ImageJFunctions.show(source, "Input");
+		
+		long[] dims = Tools.dimensions(source);
+		System.out.println("Dims: " + Util.printCoordinates(dims));
 
+		int[] blockSize  = new int[dims.length];
+		Arrays.fill(blockSize , 80);
+		System.out.println("Blocks: " + Util.printCoordinates(blockSize));
+		
+		N5Writer n5 = new N5FSWriter(output_path);
+		final DatasetAttributes attributes = new DatasetAttributes(
+				dims,
+				blockSize,
+				N5Utils.dataType(Util.getTypeFromInterval(source)),
+				new GzipCompression());
+
+		String dataset = "/volumes/raw" ;
+		n5.createDataset(dataset , attributes);
+		System.out.println("dataset created : "+ output_path);
+		
+		RandomAccessibleInterval<FloatType> virtual = N5Utils.open(new N5FSReader(output_path), dataset);
+		ImageJFunctions.show(virtual,"Black output");
+
+		saveBlock(0,source,n5,dataset,output_path,new long[] {1,1,1},new long[] {80,80,80},new long[] {0,0,0});
+
+		saveBlock(2,source,n5,dataset,output_path,new long[] {1,81,1},new long[] {80,160,80},new long[] {0,1,0});
+
+
+		saveBlock(3,source,n5,dataset,output_path,new long[] {81,81,1},new long[] {160,160,80},new long[] {1,1,0});
+
+		/*
 		long[] dims = Tools.dimensions(source);
 		MyLogger.log.info("Dims: " + Util.printCoordinates(dims));
 
@@ -97,6 +135,18 @@ public class TestSaveSpimInBlocksN5 {
 			N5IO.show(output,i+"- output");
 	
 		}
+*/
+	}
+	
+	private static void saveBlock(int i, RandomAccessibleInterval<FloatType> source, N5Writer n5, String dataset,
+			String output_path, long[] ls, long[] ls2, long[] grid) throws IOException {
+		RandomAccessibleInterval< FloatType > block0 = Views.interval( source,ls,ls2);
+		ImageJFunctions.show(block0,"block "+i);
 
+		N5Utils.saveBlock(block0, n5, dataset,  grid);
+		
+		RandomAccessibleInterval<FloatType> virtual2 = N5Utils.open(new N5FSReader(output_path), dataset);
+		
+		ImageJFunctions.show(virtual2,"After block "+i+" output");
 	}
 }
