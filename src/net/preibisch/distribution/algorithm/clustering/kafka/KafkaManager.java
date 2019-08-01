2e5a4d6c6be423b75c6e5b4bed1be88c1c7ac78a
Marwan <zouinkhi.marwan@gmail.com>
2019-08-01 17:28:50 +0200

    fix msg with block info

--- src/net/preibisch/distribution/algorithm/clustering/kafka/KafkaManager.java
+++ src/net/preibisch/distribution/algorithm/clustering/kafka/KafkaManager.java
@@ -5,20 +5,20 @@ import java.util.Timer;
 import net.preibisch.distribution.algorithm.controllers.items.Job;
 
 public class KafkaManager {
-	public static void log(String msg) {
-		send(KafkaMessage.LOG, msg);
+	public static void log(int block, String msg) {
+		send(KafkaMessage.LOG, block, msg);
 	}
 
-	public static void error(String msg) {
-		send(KafkaMessage.ERROR, msg);
+	public static void error(int block, String msg) {
+		send(KafkaMessage.ERROR, block, msg);
 	}
 
-	public static void done(String msg) {
-		send(KafkaMessage.DONE, msg);
+	public static void done(int block, String msg) {
+		send(KafkaMessage.DONE, block, msg);
 	}
 
-	private static void send(KafkaMessage type, String msg) {
-		String message = Job.getId() + ";" + msg;
+	private static void send(KafkaMessage type, int block, String msg) {
+		String message = Job.getId() + ";" + block + ";" + msg;
 		String topic = KafkaMessage.getTopic(type);
 		TaskDoneProducer producerThread = new TaskDoneProducer(topic, message);
 		new Timer().schedule(producerThread, 1000);
